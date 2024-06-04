670300faab1cad0c1ecdd6803d40777ff3e9554d
Mars Ponce <marcelo.ponce.ardon@gmail.com>
2024-06-04 15:35:47 -0400

    Implemented some basic CSS for the nav bar

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -2,37 +2,32 @@ import '../App.css';
 
 // Navigation bar component
 
-// Hide the other components except for the one clicked on 
-function Navigate(pageName) {
-	console.log('navigated to: ' + pageName);
+// Navigate to the 'page' with the given name
+function Navigate(name) {
+	console.log('navigated to: ' + name);
 	const components = document.getElementsByClassName("App-component");
 	for (let i = 0; i < components.length; i++) {
 		components[i].style.display = "none";
 	}
-	document.getElementById(pageName).style.display = "block";
+	const selected = document.getElementById(name);
+	selected.style.display = "block";
+
 }
 
 function Nav() {
 	return (
-		<nav className="App-navigation">
-			<h3>Jules Ferguson</h3>
-			<span className="Nav-links">
+		<nav className="Navigation">
+			<div className="Nav-Buttons">
+				<a id="Title" href="#home" onClick={() => Navigate("Home")}>Jules Ferguson</a>
 				<a href="#home" onClick={() => Navigate("Home")}>Home</a>
 				<a href="#about" onClick={() => Navigate("About")}>About</a>
 				<a href="#community-work" onClick={() => Navigate("CommunityWork")}>Community Work</a>
 				<a href="#contact" onClick={() => Navigate("Contact")}>Contact</a>
 				<a href="#cv" onClick={() => Navigate("CV")}>CV</a>
 				<a href="#gallery" onClick={() => Navigate("Gallery")}>Gallery</a>
-			</span>
+			</div>
 		</nav>
 	);
 }
-
-// By default start on the home page
-const components = document.getElementsByClassName("App-component");
-for (let i = 0; i < components.length; i++) {
-	components[i].style.display = "none";
-}
-document.getElementById("Home").style.display = "block";
 
 export default Nav;
