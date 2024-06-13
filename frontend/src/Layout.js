d25f4bd8d600fecfac44b46d6b7349c713fc7fb1
Mars Ponce <marcelo.ponce.ardon@gmail.com>
2024-06-13 19:43:10 -0400

    Installed CSSTransition and TransitionGroup

--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -1,13 +1,19 @@
 import { Header, Footer, Nav } from './components';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 function Layout() {
+	const location = useLocation();
   return (
 		<>
     <div className="App">
 			<Header />
 			<Nav />
-			<Outlet />
+			<TransitionGroup>
+				<CSSTransition>
+					<Outlet />
+				</CSSTransition>
+			</TransitionGroup>
 			<Footer />
     </div>
 		</>
