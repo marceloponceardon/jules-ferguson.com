a053a9460d2d35f1f6102ecdf5726f82ac6878c3
Mars Ponce <marcelo.ponce.ardon@gmail.com>
2024-06-21 16:01:09 -0400

    Almost finished styling contact form

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,13 +1,33 @@
 // Contact Page Component
+
+
+
 function Contact() {
 	return (
 		<div className="Page" id="contact">
-			<h2>Contact Me</h2>
-			<p>
-				You can contact me through my email or phone number. I am also available
-				on social media.	
-			</p>
+				<span className="Content">
+				<div className="Flex-gap"></div>
+				<p>
+					<h2>Contact Me</h2>
+					Insert some flavour text here Jules.
+				</p>
+				<form className="contact-form">
+					<input type="text" name="first-name" id="first-name" />
+					<label id="first-name">First Name</label>
+	
+					<input type="text" name="last-name" id="last-name" />
+					<label id="last-name">Last Name</label>
+
+					<input type="text" name="email" id="email" />
+					<label id="email">Email</label>
+
+					<textarea name="message" id="message" rows="5" />
+					<label id="message">Message</label>
 			
+					<input type="submit" value="Submit" />
+				</form>
+				<div className="Flex-gap"></div>
+			</span>
 		</div>
 	);
 }
