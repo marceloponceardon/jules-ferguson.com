243a54c7ed7772b8a5fca3dec341ae1ae5044a94
Mars Ponce <marcelo.ponce.ardon@gmail.com>
2024-06-25 11:55:07 -0400

    Started working on the BE

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dotenv = require('dotenv');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var cvRouter = require('./routes/cv');
+var contactRouter = require('./routes/contact');
+
+// Load env vars
+dotenv.config();
 
 var app = express();
 
@@ -13,14 +18,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/cv', cvRouter);
+app.use('/contact', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +45,11 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// Some info
+console.log('App is running in: ' + process.env.ENV + ' mode');
+console.log('\tport: ' + process.env.PORT);
+console.log('\tFE: ' + process.env.FRONTEND_URL);
+console.log('\tBE: ' + process.env.BACKEND_URL);
 
 module.exports = app;
---
+++ backend/routes/contact.js
@@ -0,0 +1,34 @@
+var express = require('express'); 
+var router = express.Router();
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+router.post('/', async (req, res) => {
+    const { name, email, message } = req.body;
+
+    let transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    let mailOptions = {
+        from: process.env.EMAIL_USER,
+        replyTo: email,
+        to: process.env.EMAIL_TO,
+        subject: `Contact Form Submission from ${name}`,
+        text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).send('Email sent successfully');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error sending email');
+    }
+});
+
+module.exports = router;
