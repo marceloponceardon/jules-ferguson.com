545117eb27db8f0a72caa374b6a6ba570646a7f9
Mars Ponce <marcelo.ponce.ardon@gmail.com>
2024-06-26 17:03:12 -0400

    3 setup be application (#6)
    * Started working on the BE
    
    * Forgot to remove node_modules
    
    * Removed node_modules
    
    * Addded nodemailer for sending contact form submissions
    
    * Added contact and cv routes functionality
    
    * Updated README.md and removed index router and index route
    
    * Update README.md
    
    * Update README.md
    
    * remove .env.example from src

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dotenv = require('dotenv');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var cvRouter = require('./routes/cv');
+var contactRouter = require('./routes/contact');
+
+// Load env vars
+dotenv.config();
 
 var app = express();
 
@@ -13,14 +17,19 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Routes
+app.use('/cv', cvRouter);
+app.use('/contact', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +46,13 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// Some info
+console.log('App is running in: ' + process.env.NODE_ENV + ' mode');
+console.log('\tport: ' + process.env.PORT);
+console.log('\tFE: ' + process.env.FRONTEND_URL);
+console.log('\tBE: ' + process.env.BACKEND_URL);
+console.log('\tEMAIL_USER: ' + process.env.EMAIL_USER);
+console.log('\tEMAIL_TO: ' + process.env.EMAIL_TO);
 
 module.exports = app;
---
+++ backend/routes/contact.js
@@ -0,0 +1,36 @@
+var express = require('express'); 
+var router = express.Router();
+var nodemailer = require('nodemailer');
+require('dotenv').config();
+
+router.post('/', async (req, res) => {
+    const { name, email, message } = req.body;
+
+    let transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    let mailOptions = {
+        from: process.env.EMAIL_USER,
+        replyTo: email,
+        to: process.env.EMAIL_TO,
+        subject: `Contact Form Submission from ${name}`,
+        text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).send('Email sent successfully');
+    } catch (error) {
+				// Include error in response body for debugging
+        console.error(error);
+				res.status(500);
+				res.send(error);
+    }
+});
+
+module.exports = router;
