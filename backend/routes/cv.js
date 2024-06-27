7de704a7cb3c3c7dd0804c17e2ec2c927ce13517
Mars Ponce <marcelo.ponce.ardon@gmail.com>
2024-06-27 18:44:45 -0400

    8 implement cv download page (#10)
    * Added download CV button with some styling
    
    * Added /cv/download route for file downloading
    
    * Implemented CV page
    
    * Implemented CV disply page
    
    * Added try-catch around file access to prevent crashing when file not found

--- backend/routes/cv.js
+++ backend/routes/cv.js
@@ -1,12 +1,44 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var fs = require('fs');
 require('dotenv').config();
 
-router.get('/', async (req, res) => {
+// Serve the cv.pdf file as a download
+router.get('/download', async (req, res) => {
+	// Dummy wait to simulate a slow download for testing
+	if (process.env.NODE_ENV === 'development') {
+		await new Promise(resolve => setTimeout(resolve, 2000));
+	}
 	const filePath = path.join(__dirname, '..', 'public', 'cv', 'cv.pdf');
-	console.log(filePath);
-	res.sendFile(filePath);
+	try {
+		fs.accessSync(filePath, fs.constants.R_OK);
+	}	catch (err) {
+		console.error(err);
+		res.status(404).send('File not found');
+		return;
+	}
+	const file = fs.createReadStream(filePath);
+	res.setHeader('Content-Disposition', 'attachment; filename=jules-ferguson-cv.pdf');
+	res.setHeader('Content-Type', 'application/pdf');
+	file.pipe(res);
 });
+
+// Serve the cv.pdf file to be viewed in the browser in an iframe
+// This is used in the contact page
+router.get('/view', async (req, res) => {
+	const filePath = path.join(__dirname, '..', 'public', 'cv', 'cv.pdf');
+	try {
+		fs.accessSync(filePath, fs.constants.R_OK);
+	}	catch (err) {
+		console.error(err);
+		res.status(404).send('File not found');
+		return;
+	}
+	const file = fs.createReadStream(filePath);
+	res.setHeader('Content-Type', 'application/pdf');
+	file.pipe(res);
+});
+
 
 module.exports = router;
--- frontend/src/pages/CV.js
+++ frontend/src/pages/CV.js
@@ -1,9 +1,103 @@
 // CV component
+import { toast } from 'react-toastify';
+import { pdfjs } from 'react-pdf';
+import { useState } from 'react';
+import { Document, Page } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+	'pdfjs-dist/build/pdf.worker.min.mjs',
+	import.meta.url,
+).toString();
+
 function CV() {
+
+	// URL for API calls related to CV
+	const cvURL = process.env.REACT_APP_BACKEND_URL + "/cv";
+	
+	const handleDownload = async (response) => {
+		const downloadToast = toast.loading('Downloading CV...');	
+		// Make API call to download CV
+		try {
+			if (process.env.NODE_ENV === 'development') {
+				console.log('URI:', cvURL + '/download');
+			}
+
+			const response = await fetch(cvURL + '/download', {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/pdf',
+				},
+			});
+
+			if (response.ok) {
+				const blob = await response.blob();
+				const url = window.URL.createObjectURL(blob);
+				const a = document.createElement('a');
+				a.href = url;
+				a.download = 'jules-ferguson-cv.pdf';
+				a.click();
+				toast.update(downloadToast, {
+					type: 'success',
+					render: 'CV downloaded successfully',
+					isLoading: false,
+					closeButton: true
+				});
+			} else {
+				toast.update(downloadToast, {
+					render: 'Failed to download CV! (' + response.status + ')',
+					type: 'error',
+					isLoading: false,
+					closeButton: true
+				});
+			}
+		} catch (error) {
+			console.error('Fetch error:', error);
+			toast.update(downloadToast, {
+				render: 'Failed to download CV',
+				type: 'error',
+				isLoading: false,
+				closeButton: true
+			});
+		}
+	};
+
+	// PDF viewer
+	const [numPages, setNumPages] = useState(null);
+	const [pageNumber, setPageNumber] = useState(1);
+
+	function onDocumentLoadSuccess({ numPages }) {
+		setNumPages(numPages);
+	}
+
 	return (
 		<div className="Page" id="cv">
-			<h2>CV</h2>
-			<p>This is a paragraph</p>
+			<span className="Content">
+				<h2>CV</h2>
+				<div className="CV-Container">
+					<Document 
+						file={cvURL + '/view'}
+						onLoadSuccess={onDocumentLoadSuccess}
+						onLoadFailure={(error) => console.error('PDF load error:', error)}
+					>
+						<Page pageNumber={pageNumber} />
+					</Document>
+				</div>
+				{/* Page number and next and previous buttons */}
+				<p>Page {pageNumber} of {numPages}</p>
+				<button className="CV-Button" id="previous"
+					disabled={pageNumber <= 1}
+					onClick={() => setPageNumber(pageNumber - 1)}
+				>Previous</button>
+				<button className="CV-Button" id="next"
+					disabled={pageNumber >= numPages}
+					onClick={() => setPageNumber(pageNumber + 1)}
+				>Next</button>
+				<br />
+				{/* Download button */}
+				<button onClick={handleDownload}>
+					Download as PDF
+				</button>
+			</span>
 		</div>
 	);
 }
